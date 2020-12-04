a001c505a5f8ef5cf64507a11471bdb0bc3119a9
Marc Schaake <marc.schaake@thinkproject.com>
2020-12-04 16:27:03 +0100

    add webdav

---
+++ user_test.go
@@ -0,0 +1,30 @@
+package movielight
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/steinfletcher/apitest"
+	jsonpath "github.com/steinfletcher/apitest-jsonpath"
+)
+
+func TestUserGet(t *testing.T) {
+	apitest.New(). // configuration
+			Handler(S.Router).
+			Get("/api/user").
+			Expect(t).
+			Body(`[{"UserName":"test", "id":1}]`).
+			Assert(jsonpath.GreaterThan(`$`, 1)).
+			Status(http.StatusOK).
+			End()
+}
+
+func TestUserGetOne(t *testing.T) {
+	apitest.New(). // configuration
+			Handler(S.Router).
+			Get("/api/user/1").
+			Expect(t).
+			Body(`{"UserName":"test", "id":1}`).
+			Status(http.StatusOK).
+			End()
+}
---
+++ webdav.go
@@ -0,0 +1,58 @@
+package movielight
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
+	"golang.org/x/net/webdav"
+)
+
+// ANY /webdav/*
+func WebDAV(path string, router *gin.RouterGroup) {
+	if router == nil {
+		log.Error("webdav: router is nil")
+		return
+	}
+
+	// if conf == nil {
+	// 	log.Error("webdav: conf is nil")
+	// 	return
+	//	}
+
+	f := webdav.Dir(path)
+
+	srv := &webdav.Handler{
+		Prefix:     router.BasePath(),
+		FileSystem: f,
+		LockSystem: webdav.NewMemLS(),
+		Logger: func(r *http.Request, err error) {
+			if err != nil {
+				log.Errorf("webdav: %s %s, ERROR: %s\n", r.Method, r.URL, err)
+			} else {
+				log.Debugf("webdav: %s %s \n", r.Method, r.URL)
+			}
+		},
+	}
+
+	handler := func(c *gin.Context) {
+		w := c.Writer
+		r := c.Request
+
+		srv.ServeHTTP(w, r)
+	}
+
+	router.Handle("OPTIONS", "/*path", handler)
+	router.Handle("GET", "/*path", handler)
+	router.Handle("HEAD", "/*path", handler)
+	router.Handle("POST", "/*path", handler)
+	router.Handle("DELETE", "/*path", handler)
+	router.Handle("PUT", "/*path", handler)
+	router.Handle("MKCOL", "/*path", handler)
+	router.Handle("COPY", "/*path", handler)
+	router.Handle("MOVE", "/*path", handler)
+	router.Handle("LOCK", "/*path", handler)
+	router.Handle("UNLOCK", "/*path", handler)
+	router.Handle("PROPFIND", "/*path", handler)
+	router.Handle("PROPPATCH", "/*path", handler)
+}
