ed50a20e8aafd4d4c9d4d96ef0a56a354109cdb7
Marc Schaake <marc.schaake@thinkproject.com>
2020-12-31 12:06:40 +0100

    remove bootstrap

--- movieui/src/main.js
+++ movieui/src/main.js
@@ -8,17 +8,8 @@ import axios from 'axios'
 import lodash from 'lodash'
 import MobileDetect from 'mobile-detect'
 import VeeValidate from 'vee-validate'
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { library } from '@fortawesome/fontawesome-svg-core'
+
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import {
-  faHome,
-  faUser,
-  faUserPlus,
-  faSignInAlt,
-  faSignOutAlt
-} from '@fortawesome/free-solid-svg-icons'
 
 import './assets/main.css'
 
@@ -56,7 +47,6 @@ Vue.filter('truncate', function (text, stop, clamp) {
   return text.substr(0, text.lastIndexOf(' ', stop)) + ' ...'
 })
 
-library.add(faHome, faUser, faUserPlus, faSignInAlt, faSignOutAlt)
 Vue.use(VeeValidate)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 new Vue({
