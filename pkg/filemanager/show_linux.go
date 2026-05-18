5ca0459c5e814623ed006b082bce7ce9cf4563b1
Marc Schaake <ms@alter-fisch.de>
2026-05-18 23:32:26 +0200

    Refactor file manager detection and show logic on Linux

--- pkg/filemanager/show_linux.go
+++ pkg/filemanager/show_linux.go
@@ -12,113 +12,21 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-func detectFileManager() (string, bool) {
+func detectFileManager() string {
 	desktop := strings.ToLower(os.Getenv("XDG_CURRENT_DESKTOP"))
 
 	managerMap := map[string]string{
-		"gnome": "gio",
-		"unity": "gio",
-		"ubuntu": "gio",
-		"xfce4": "exo-open",
-		"mate": "gio",
+		"gnome":   "gio",
+		"unity":   "gio",
+		"ubuntu":  "gio",
+		"xfce4":   "exo-open",
+		"mate":    "gio",
 		"cinnamon": "gio",
-		"kde": "dolphin",
-		"plasma": "dolphin",
-		"kxfce": "exo-open",
+		"kde":     "xdg-open",
+		"plasma":  "xdg-open",
 	}
 
-	for _, desk := range []string{"gnome", "unity", "ubuntu", "xfce4", "mate", "cinnamon", "kde", "plasma", "kxfce"} {
-		if strings.Contains(desktop, desk) {
-			if manager, ok := managerMap[desk]; ok {
-				return manager, true
-			}
-		}
-	}
-
-	return "xdg-open", false
-}
-
-	for _, desk := range []string{"gnome", "unity", "ubuntu", "xfce4", "mate", "cinnamon", "kde", "plasma", "kxfce"} {
-		if strings.Contains(desktop, desk) {
-			if manager, ok := managerMap[desk]; ok {
-				return manager, true
-			}
-		}
-	}
-
-	return "xdg-open", false
-}
-
-func showViaCMD(args ...string) error {
-	fullCmd := exec.Command(args[0], args[1:]...)
-	return fullCmd.Start()
-}
-
-func showFile(path string) error {
-	manager, _ := detectFileManager()
-
-	switch manager {
-	case "gio":
-		return showViaCMD("gio", "open", path)
-	case "dolphin":
-		return showViaCMD("dolphin", "--select", path)
-	case "nautilus":
-		return showViaCMD("nautilus", "--select", path)
-	case "thunar":
-		return showViaCMD("thunar", "--select", path)
-	case "konqueror":
-		return showViaCMD("konqueror", "--select", path)
-	case "spacefm":
-		return showViaCMD("spacefm", "--select", path)
-	case "pcmanfm":
-		return showViaCMD("pcmanfm", "--select", path)
-	case "exo-open":
-		return showViaCMD("exo-open", "--launch", "FileManager", path)
-	default:
-		return showViaCMD("xdg-open", path)
-	}
-}
-
-func ShowDir(path string) error {
-	dir := filepath.Dir(path)
-
-	if path != dir {
-		return showFile(path)
-	}
-
-	manager, _ := detectFileManager()
-
-	var err error
-	switch manager {
-	case "gio":
-		err = showViaCMD("gio", "open", dir)
-	case "dolphin":
-		err = showViaCMD("dolphin", dir)
-	case "nautilus":
-		err = showViaCMD("nautilus", dir)
-	case "thunar":
-		err = showViaCMD("thunar", dir)
-	case "konqueror":
-		err = showViaCMD("konqueror", dir)
-	case "spacefm":
-		err = showViaCMD("spacefm", dir)
-	case "pcmanfm":
-		err = showViaCMD("pcmanfm", dir)
-	case "exo-open":
-		err = showViaCMD("exo-open", "--launch", "FileManager", dir)
-	default:
-		err = showViaCMD("xdg-open", dir)
-	}
-
-	if err != nil {
-		log.Errorf("failed to show directory %s: %v", dir, err)
-		return err
-	}
-
-	return nil
-}
-
-	for _, desk := range []string{"gnome", "unity", "ubuntu", "xfce4", "mate", "cinnamon", "kde", "plasma", "kxfce"} {
+	for _, desk := range []string{"gnome", "unity", "ubuntu", "xfce4", "mate", "cinnamon", "kde", "plasma"} {
 		if strings.Contains(desktop, desk) {
 			if manager, ok := managerMap[desk]; ok {
 				return manager
@@ -134,18 +42,31 @@ func showViaCMD(args ...string) error {
 	return fullCmd.Start()
 }
 
-func ShowDir(dir string) error {
-	manager := detectFileManager()
+func ShowDir(path string) error {
+	dir := filepath.Dir(path)
 
 	var err error
+	manager := detectFileManager()
 
 	switch manager {
 	case "gio":
-		err = showViaCMD("gio", "open", dir)
+		err = showViaCMD("gio", "open", path)
+	case "nautilus":
+		err = showViaCMD("nautilus", path)
+	case "thunar":
+		err = showViaCMD("thunar", path)
+	case "dolphin":
+		err = showViaCMD("dolphin", path)
+	case "konqueror":
+		err = showViaCMD("konqueror", path)
+	case "spacefm":
+		err = showViaCMD("spacefm", path)
+	case "pcmanfm":
+		err = showViaCMD("pcmanfm", path)
 	case "exo-open":
-		err = showViaCMD("exo-open", "--launch", "FileManager", dir)
+		err = showViaCMD("exo-open", "--launch", "FileManager", path)
 	default:
-		err = showViaCMD("xdg-open", dir)
+		err = showViaCMD("xdg-open", path)
 	}
 
 	if err != nil {
